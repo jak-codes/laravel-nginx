7675c3b80ac91afdb09864ab016ef346ffa72e67
jak-codes <93322856+jak-codes@users.noreply.github.com>
2021-11-07 21:06:32 +0700

    update nginx

--- laravel-nginx.sh
+++ laravel-nginx.sh
@@ -7,17 +7,14 @@ sudo apt-get install nginx -y
 #configuration nginx
 sudo cat << EOF > /etc/nginx/sites-available/default
 server {
-        listen 80 default_server;
-        listen [::]:80 default_server;
-
+        listen 80;
+        server_name _;
 	root /var/www/html;
-	
+
         index index.html index.htm index.nginx-debian.html index.php;
 
-        server_name _;
-
         location / {
-                try_files $uri $uri/ /index.php?$query_string;
+		try_files $uri $uri/ /index.php?$query_string;
         }
 
         location ~ \.php$ {
@@ -25,10 +22,11 @@ server {
                 fastcgi_pass unix:/var/run/php/php8.0-fpm.sock;
         }
 }
+
 EOF
 
 #install php8.0 with extensions
-add-apt-repository ppa:ondrej/php
+sudo add-apt-repository ppa:ondrej/php -y
 grep ^ /etc/apt/sources.list /etc/apt/sources.list.d/* | grep ondrej/php
 sudo apt-get update
 sudo apt install php8.0-{common,cli,bcmath,curl,dev,fpm,gd,mbstring,mysql,opcache,pgsql,xml,zip} -y
